0388747a04e90a2d0bd249c147410437d26ea6b8
Vedant Malhotra <vedantapps@gmail.com>
2021-11-24 19:39:04 -0800

    Initial Commit

---
+++ MultiScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiScreen
+//
+//  Created by Vedant Malhotra on 11/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
